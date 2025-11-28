be995e6f60657f050a7bc6cb443960ec0cf8f0ee
scorpion-varun <varuningle5@gmail.com>
2025-11-29 03:51:21 +0530

    Resolve deplyment issues

--- src/admin/pages/AdminLoginPage.tsx
+++ src/admin/pages/AdminLoginPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AdminLoginPage = () => {
   return <div>AdminLoginPage</div>;
 };
--- src/pages/AppointmentPage.tsx
+++ src/pages/AppointmentPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AppointmentPage = () => {
   return <div>AppointmentPage</div>;
 };
