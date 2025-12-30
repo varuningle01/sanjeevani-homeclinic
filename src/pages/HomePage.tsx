4a56046b1b8628b846cff0e0d2a6adb1d7fab445
scorpion-varun <varuningle5@gmail.com>
2025-12-30 19:44:57 +0530

    Resolved Images issue

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,35 +8,35 @@ import fallbackValues from "../locales/fallback-values.json";
 const serviceData = [
   {
     key: "one",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
   {
     key: "two",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
   {
     key: "three",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
   {
     key: "four",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
   {
     key: "five",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
   {
     key: "six",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
   {
     key: "seven",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
   {
     key: "eight",
-    image: "https:placehold.co/800?text=Services+Images&font=roboto",
+    image: "https://placehold.co/800?text=Services+Images&font=roboto",
   },
 ];
 function HomePage() {
