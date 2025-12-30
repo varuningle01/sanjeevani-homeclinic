665b6658a2c2061ae714c2b2e9f00e329752b0fa
scorpion-varun <varuningle5@gmail.com>
2025-12-30 19:39:19 +0530

    resolving vercel deployment issues

--- src/admin/services/patientService.ts
+++ src/admin/services/patientService.ts
@@ -6,7 +6,6 @@ import type {
   AddVisitData,
   UpdateVisitData,
   PatientVisit,
-  MedicalReport,
 } from "../types/patient.types";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
